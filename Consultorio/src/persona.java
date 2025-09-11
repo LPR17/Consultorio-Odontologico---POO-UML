735c2717aa209919515714accacbf754ae675987
peerre <lautaroprojas@gmail.com>
2025-09-11 11:31:08 -0300

    Definición clase abstracta persona

---
+++ Consultorio/src/persona.java
@@ -0,0 +1,14 @@
+public abstract class persona {
+    protected String nombre;
+    protected String apellido;
+    protected int dni;
+
+
+    public persona(String nombre, String apellido, int dni){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+
+}
